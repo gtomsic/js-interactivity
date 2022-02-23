c89efd608b0c8dc29ed8d616eabbea37a9547d69
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-23 14:12:42 -0700

    Delete movies variable

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
-const movies = [];
 
 const addMovie = (e) => {
   e.preventDefault();
