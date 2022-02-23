c191c52041b37e5c8002e94c09121e6b1c09eec0
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-23 14:42:57 -0700

    Custom message done

--- index.js
+++ index.js
@@ -16,19 +16,32 @@ const addMovie = (e) => {
   li.addEventListener('click', deleteMovie);
   ul.prepend(li);
   input.value = '';
+  customMessage('Movie title added!');
 };
 
 const deleteMovie = (e) => {
+  e.stopPropagation();
   e.target.parentNode.remove();
+  customMessage('Movie deleted!');
 };
 
 const crossOutWord = (e) => {
   e.stopPropagation();
+
   if (e.target.classList.contains('checked')) {
     e.target.classList.remove('checked');
     return;
   }
   e.target.className = 'checked';
+  customMessage('Word crossed out!');
+};
+
+const customMessage = (message) => {
+  const messageEl = document.getElementById('message');
+  messageEl.textContent = message;
+  setTimeout(() => {
+    messageEl.className = 'hide';
+  }, 1000);
 };
 
 form.addEventListener('submit', addMovie);
